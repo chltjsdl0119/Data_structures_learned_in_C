39eb6afaf1941a154fc946813d04195ff7bdf94d
Seungwon_Choi <bestwondev@gmail.com>
2024-05-07 00:55:58 +0900

    Linear list

---
+++ DataStructure/listS.h
@@ -0,0 +1,9 @@
+//
+// Created by 최승원 on 24. 5. 7.
+//
+
+#pragma once
+#define MAX 10
+
+int insertElement(int L[], int n, int x);
+int deleteElement(int L[], int n, int x);
--- DataStructure/main.c
+++ DataStructure/main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include "listS.h"
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+
+    int list[MAX] = {10, 20, 30, 40, 50};
+
+    int i, move, size = 5;
+
+    printf("\nBefore insert: ");
+    for (i = 0; i < size; i++)
+        printf("%d ", list[i]);
+
+    printf("\nElement Count: %d\n", size);
+
+    move = insertElement(list, size, 25);
+
+    printf("\nAfter insert: ");
+    for (i = 0; i < size + 1; i++)
+        printf("%d ", list[i]);
+
+    printf("\nElement Count: %d", size + 1);
+    printf("\nMove Count: %d\n", move);
+
+    move = deleteElement(list, size, 30);
+
+    if (move == 0) {
+        printf("\nDelete Failed\n");
+    } else {
+        printf("\nAfter delete: ");
+        for (i = 0; i < size; i++)
+            printf("%d ", list[i]);
+
+        printf("\nElement Count: %d", size - 1);
+        printf("\nMove Count: %d\n", move);
+    }
+
     return 0;
 }
