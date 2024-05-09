de5f90b231446e613c3218cda5e0b885f1934b07
Seungwon_Choi <bestwondev@gmail.com>
2024-05-10 00:26:45 +0900

    Single Linked List

---
+++ DataStructure/InsertLinkedList.c
@@ -0,0 +1,78 @@
+//
+// Created by 최승원 on 24. 5. 10.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "InsertLinkedList.h"
+
+linkedList_h* createLinkedList_h(void) {
+    linkedList_h* L;
+    L = (linkedList_h*)malloc(sizeof(linkedList_h));
+    L -> head = NULL;
+    return L;
+}
+
+void freeLinkedList_h(linkedList_h* L) {
+    listNode* p;
+    while (L -> head != NULL) {
+        p = L -> head;
+        L -> head = L -> head -> link;
+        free(p);
+        p = NULL;
+    }
+}
+
+void printList(linkedList_h* L) {
+    listNode* p;
+    printf("L = (");
+    p = L -> head;
+    while (p != NULL) {
+        printf("%s", p -> data);
+        p = p -> link;
+        if (p != NULL)
+            printf(", ");
+    }
+    printf(")\n");
+}
+
+void insertFirstNode(linkedList_h* L, char* x) {
+    listNode* newNode;
+    newNode = (listNode*)malloc(sizeof(listNode));
+    strcpy(newNode -> data, x);
+    newNode -> link = L -> head;
+    L -> head = newNode;
+}
+
+void insertMiddleNode(linkedList_h* L, listNode* pre, char* x) {
+    listNode* newNode;
+    newNode = (listNode*)malloc(sizeof(listNode));
+    strcpy(newNode -> data, x);
+    if (L -> head == NULL) {
+        newNode -> link = NULL;
+        L -> head = newNode;
+    } else if (pre == NULL) {
+        free(newNode);
+        printf("error\n");
+    } else {
+        newNode -> link = pre -> link;
+        pre -> link = newNode;
+    }
+}
+
+void insertLastNode(linkedList_h* L, char* x) {
+    listNode* newNode;
+    listNode* p;
+    newNode = (listNode*)malloc(sizeof(listNode));
+    strcpy(newNode -> data, x);
+    newNode -> link = NULL;
+    if (L -> head == NULL) {
+        L -> head = newNode;
+    } else {
+        p = L -> head;
+        while (p -> link != NULL)
+            p = p -> link;
+        p -> link = newNode;
+    }
+}
--- DataStructure/main.c
+++ DataStructure/main.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
-#include "addPoly.h"
+#include "InsertLinkedList.h"
 
 int main() {
 
-    polynomial A = {3, {4, 3, 5, 0}};
-    polynomial B = {4, {3, 1, 0, 2, 1}};
-    polynomial C;
+    linkedList_h* L;
+    listNode* p;
+    L = createLinkedList_h();
+    printf("(1) 공백 리스트 생성하기! \n");
+    printList(L);
 
-    C = addPoly(A, B);
+    printf("(2) 리스트에 [월] 노드 삽입하기! \n");
+    insertFirstNode(L, "월");
+    printList(L);
 
-    printf("A(x) = ");
-    printPoly(A);
+    printf("(3) 리스트의 [월] 노드 뒤에 [수] 노드 삽입하기! \n");
+    p = L -> head;
+    insertMiddleNode(L, p, "수");
+    printList(L);
 
-    printf("B(x) = ");
-    printPoly(B);
+    printf("(4) 리스트의 [수] 노드 뒤에 [금] 노드 삽입하기! \n");
+    p = p -> link;
+    insertMiddleNode(L, p, "금");
+    printList(L);
 
-    printf("C(x) = ");
-    printPoly(C);
+    printf("(5) 리스트의 [수] 노드 뒤에 [목] 노드 삽입하기! \n");
+    p = p -> link;
+    insertMiddleNode(L, p, "목");
+    printList(L);
+
+    printf("\n(6) 리스트 공간을 해제하여 공백 리스트로 만들기! \n");
+    freeLinkedList_h(L);
+    printList(L);
 
     return 0;
 }
